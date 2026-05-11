a13f25368e2b9763d22d98fcf6fcec4d71277d24
Anushka Singh <ai.engineer.anushka@gmail.com>
2026-05-11 21:44:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ tfidf = pickle.load(open("tfidf.pkl", "rb"))
 # Page Configuration
 # =========================================================
 st.set_page_config(
-    page_title="Emotion Detector AI",
+    page_title="Emotion Detector ",
     page_icon="💬",
     layout="centered"
 )
